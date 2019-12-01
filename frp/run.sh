f516094c0a0b63344f76decf7999404163442845
zhangwenchao1992 <878829744@qq.com>
2019-12-01 21:14:58 +0800

    v1

---
+++ frp/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+CONFIG_PATH=/data/options.json
+
+SERVER_IP=$(jq --raw-output '.server_addr' $CONFIG_PATH)
+AUTH_TOKEN=$(jq --raw-output '.token'  $CONFIG_PATH)
+SERVER_PORT=$(jq --raw-output '.server_port' $CONFIG_PATH)
+LOCAL_PORT=$(jq --raw-output '.local_port' $CONFIG_PATH)
+REMOTE_PORT=$(jq --raw-output '.remote_port' $CONFIG_PATH)
+PROXY_NAME=$(jq --raw-output '.proxy_name // empty' $CONFIG_PATH)
+SUBDOMAIN=$(jq --raw-output '.subdomain' $CONFIG_PATH)
+
+FRP_PATH=/var/frp
+FRPC_CONF=$FRP_PATH/conf/frpc.ini
+
+if [ -f $FRPC_CONF ]; then
+  rm $FRPC_CONF
+fi
+
+if [ ! $PROXY_NAME ]; then
+  PROXY_NAME=hassio
+  echo Using default proxy name $PROXY_NAME
+fi
+
+echo "[common]" >> $FRPC_CONF
+echo "server_addr = $SERVER_IP" >> $FRPC_CONF
+echo "server_port = $SERVER_PORT" >> $FRPC_CONF
+echo "token = $AUTH_TOKEN" >> $FRPC_CONF
+
+echo "[$PROXY_NAME]" >> $FRPC_CONF
+echo "type = http" >> $FRPC_CONF
+echo "local_ip = 127.0.0.1" >> $FRPC_CONF
+echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
+echo "remote_port = $REMOTE_PORT" >> $FRPC_CONF
+echo "subdomain = $SUBDOMAIN" >> $FRPC_CONF
+
+echo Start frp as client
+
+exec $FRP_PATH/frpc -c $FRPC_CONF < /dev/null
